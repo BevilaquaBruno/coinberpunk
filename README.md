8d56982322a6dc3bced62da23dd6e74878b4d9de
bevilaquabruno <bbbevilaqua@gmail.com>
2021-01-22 14:54:42 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Coinberpunk page
+## this page is justo to show the current value of Bitcoin, Ethereum and Ripple.
+## The Api used is [gecko](https://www.coingecko.com/).
---
+++ main.js
@@ -0,0 +1,25 @@
+"use strict";
+function main() {
+  let html = "<div class=\"col-sm-10 no-margin no-padding\">Getting the value with Véio da Havan...</div>";
+    document.getElementById('btc_value').innerHTML = html;
+    document.getElementById('eth_value').innerHTML = html;
+    document.getElementById('xrp_value').innerHTML = html;
+    
+    document.getElementById('btc_image').setAttribute('src',"spinner.svg");
+    document.getElementById('eth_image').setAttribute('src',"spinner.svg");
+    document.getElementById('xrp_image').setAttribute('src',"spinner.svg");
+  axios({url: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=brl&ids=bitcoin,ethereum,ripple" })
+    .then(function (res) {
+      const coins = res.data;
+      coins.forEach(el => {
+        document.getElementById(el.symbol+'_value').innerHTML = "<div class=\"col-sm-12\">R$ "+el.current_price.toLocaleString('pt-BR')+"</div>";
+        document.getElementById(el.symbol+'_image').setAttribute('src', el.image);
+      });
+    }).catch(function (err) {
+      console.error(err);
+    });
+}
+
+window.onload = function () {
+  main();
+}
