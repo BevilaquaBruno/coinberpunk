a3e0011196f4f077f7a2a86b114cfdbd5f843e1b
Bruno Bevilaqua <bbbevilaqua@gmail.com>
2021-01-23 02:25:51 -0300

    remove log

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 "use strict";
 function main() {
-  console.log('foi');
   /*let html = "<div class=\"col-sm-10 no-margin no-padding\">Retrieving data from Véio da Havan...</div>";
     document.getElementById('btc_value').innerHTML = html;
     document.getElementById('eth_value').innerHTML = html;
