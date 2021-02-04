154170e8f886bb5a4792727a913a693a04020fd7
bevilaquabruno <bbbevilaqua@gmail.com>
2021-02-04 17:50:42 -0300

    language fix

--- main.js
+++ main.js
@@ -6,31 +6,31 @@ function openGithub() {
 function main() {
   axios({url: "https://api.coingecko.com/api/v3/simple/price?ids=ethereum,bitcoin,ripple,dogecoin&vs_currencies=brl,usd&include_last_updated_at=true" })
   .then(function (res) {
-    let dt;
+    let dt, lang = (navigator.language != '' && navigator.language != null)?navigator.language:'pt-BR';
     //BTC
-    document.getElementById('title_here').innerText = res.data.bitcoin.brl.toLocaleString('pt-BR')+';'+
-      res.data.ethereum.brl.toLocaleString('pt-BR')+';'+
-      res.data.ripple.brl.toLocaleString('pt-BR')+';'+
-      res.data.dogecoin.brl.toLocaleString('pt-BR');
+    document.getElementById('title_here').innerText = res.data.bitcoin.brl.toLocaleString(lang)+';'+
+      res.data.ethereum.brl.toLocaleString(lang)+';'+
+      res.data.ripple.brl.toLocaleString(lang)+';'+
+      res.data.dogecoin.brl.toLocaleString(lang);
     dt = new Date(res.data.bitcoin.last_updated_at * 1000);
-    document.getElementById('btc_value').innerHTML = "<div class=\"col-sm-12\">R$ "+res.data.bitcoin.brl.toLocaleString('pt-BR')+" | "+
-      "$ "+res.data.bitcoin.usd.toLocaleString('pt-BR')+
-      "<br>"+dt.toLocaleString('pt-BR')+"</div>";
+    document.getElementById('btc_value').innerHTML = "<div class=\"col-sm-12\">R$ "+res.data.bitcoin.brl.toLocaleString(lang)+" | "+
+      "$ "+res.data.bitcoin.usd.toLocaleString(lang)+
+      "<br>"+dt.toLocaleString(lang)+"</div>";
     //ETH
     dt = new Date(res.data.ethereum.last_updated_at * 1000);
-    document.getElementById('eth_value').innerHTML = "<div class=\"col-sm-12\">R$ "+res.data.ethereum.brl.toLocaleString('pt-BR')+" | "+
-      "$ "+res.data.ethereum.usd.toLocaleString('pt-BR')+
-      "<br>"+dt.toLocaleString('pt-BR')+"</div>";
+    document.getElementById('eth_value').innerHTML = "<div class=\"col-sm-12\">R$ "+res.data.ethereum.brl.toLocaleString(lang)+" | "+
+      "$ "+res.data.ethereum.usd.toLocaleString(lang)+
+      "<br>"+dt.toLocaleString(lang)+"</div>";
     //XRP
     dt = new Date(res.data.ripple.last_updated_at * 1000);
-    document.getElementById('xrp_value').innerHTML = "<div class=\"col-sm-12\">R$ "+res.data.ripple.brl.toLocaleString('pt-BR')+" | "+
-      "$ "+res.data.ripple.usd.toLocaleString('pt-BR')+
-      "<br>"+dt.toLocaleString('pt-BR')+"</div>";
+    document.getElementById('xrp_value').innerHTML = "<div class=\"col-sm-12\">R$ "+res.data.ripple.brl.toLocaleString(lang)+" | "+
+      "$ "+res.data.ripple.usd.toLocaleString(lang)+
+      "<br>"+dt.toLocaleString(lang)+"</div>";
     //DOGE
     dt = new Date(res.data.dogecoin.last_updated_at * 1000);
-    document.getElementById('doge_value').innerHTML = "<div class=\"col-sm-12\">R$ "+res.data.dogecoin.brl.toLocaleString('pt-BR')+" | "+
-      "$ "+res.data.dogecoin.usd.toLocaleString('pt-BR')+
-      "<br>"+dt.toLocaleString('pt-BR')+"</div>";
+    document.getElementById('doge_value').innerHTML = "<div class=\"col-sm-12\">R$ "+res.data.dogecoin.brl.toLocaleString(lang)+" | "+
+      "$ "+res.data.dogecoin.usd.toLocaleString(lang)+
+      "<br>"+dt.toLocaleString(lang)+"</div>";
     let plus = "GMT"+((dt.getHours() >= dt.getUTCHours() )? "+": "-");
     document.getElementById('current_gmt').innerText = plus+(dt.getTimezoneOffset() / 60)
   }).catch(function (err) {
